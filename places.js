8ef51f1db6691ea683cf43fe4ec7eae6eed40704
tomoponz <yuto181130+tomoponz@gmail.com>
2026-02-19 13:46:54 +0900

    Restore ドア page and add default warp places

---
+++ places.js
@@ -0,0 +1,12 @@
+window.PLACES = [
+  { wikiTitle: "Tokyo", lat: 35.6762, lng: 139.6503 },
+  { wikiTitle: "Kyoto", lat: 35.0116, lng: 135.7681 },
+  { wikiTitle: "Sapporo", lat: 43.0618, lng: 141.3545 },
+  { wikiTitle: "Osaka", lat: 34.6937, lng: 135.5023 },
+  { wikiTitle: "Naha", lat: 26.2124, lng: 127.6809 },
+  { wikiTitle: "Paris", lat: 48.8566, lng: 2.3522 },
+  { wikiTitle: "London", lat: 51.5072, lng: -0.1276 },
+  { wikiTitle: "New York City", lat: 40.7128, lng: -74.0060 },
+  { wikiTitle: "Sydney", lat: -33.8688, lng: 151.2093 },
+  { wikiTitle: "Cairo", lat: 30.0444, lng: 31.2357 }
+];
